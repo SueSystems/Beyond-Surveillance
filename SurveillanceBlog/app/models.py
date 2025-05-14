c253ede1bbe67589e30d85d56ed926f88336bb54
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-05-14 04:19:18 +0000

    Private messages support: in User model

--- SurveillanceBlog/app/models.py
+++ SurveillanceBlog/app/models.py
@@ -74,6 +74,7 @@ class User(UserMixin, db.Model):
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc))
+    last_message_read_time: so.Mapped[Optional[datetime]]
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
@@ -85,6 +86,12 @@ class User(UserMixin, db.Model):
         secondary=followers, primaryjoin=(followers.c.followed_id == id),
         secondaryjoin=(followers.c.follower_id == id),
         back_populates='following')
+    messages_sent: so.WriteOnlyMapped['Message'] = so.relationship(
+        foreign_keys='Message.sender_id', back_populates='author')
+    messages_received: so.WriteOnlyMapped['Message'] = so.relationship(
+        foreign_keys='Message.recipient_id', back_populates='recipient')
+
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -149,6 +156,15 @@ class User(UserMixin, db.Model):
         except Exception:
             return
         return db.session.get(User, id)
+
+    def unread_message_count(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        query = sa.select(Message).where(Message.recipient == self,
+                                         Message.timestamp > last_read_time)
+        return db.session.scalar(sa.select(sa.func.count()).select_from(
+            query.subquery()))
+
+
 
 
 @login.user_loader
