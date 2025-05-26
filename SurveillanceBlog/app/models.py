01f3f49b76d447de2c387513424a2459872c7329
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-05-26 19:21:52 +0000

    PaginatedAPIMixin Added to User model.

--- SurveillanceBlog/app/models.py
+++ SurveillanceBlog/app/models.py
@@ -91,7 +91,7 @@ followers = sa.Table(
 )
 
 
-class User(UserMixin, db.Model):
+class User(PaginatedAPIMixin, UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
