e8bc8ba3a6a1d78a5d1cf6877b91e1a58fcdf43f
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-05-26 19:41:40 +0000

    Return the collection of all users.

--- SurveillanceBlog/app/api/users.py
+++ SurveillanceBlog/app/api/users.py
@@ -1,4 +1,6 @@
 from app.api import bp
+import sqlalchemy as sa
+from flask import request
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
@@ -6,7 +8,10 @@ def get_user(id):
 
 @bp.route('/users', methods=['GET'])
 def get_users():
-    pass
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    return User.to_collection_dict(sa.select(User), page, per_page,
+                                   'api.get_users')
 
 @bp.route('/users/<int:id>/followers', methods=['GET'])
 def get_followers(id):
